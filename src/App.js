14af9204f70f5d5378aee2a524c0daaa2ea39d7d
llabourbeExcilys <llabourbe@excilys.com>
2019-06-13 12:29:48 +0200

    grille cocktail avec boutton delete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 
 import React, {Component} from 'react';
 import './App.css';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
-import {MOCK} from './mock';
+import RecipeDetail from "./RecipeDetail";
+import {MOCK} from "./mock";
 
 class App extends Component{
+
+    state = {
+        recipes: MOCK
+    };
+
+    delete = (id) => () => {
+        this.setState({
+            recipes: this.state.recipes.filter(item => item.id !== id)
+        })
+    };
+
     render() {
         return (
-            <Card>
-                <CardImg top width="100%" src={MOCK[0].picture} alt="Card image cap" />
-                <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <Button>Button</Button>
-                </CardBody>
-            </Card>
+            <div className="container">
+                <div className="row">
+                    {this.state.recipes.map(recipe =>
+                    <div className="col-md-3">
+                        <RecipeDetail recipe={recipe} delete={this.delete}/>
+                    </div>)}
+                </div>
+            </div>
         );
     };
 }
